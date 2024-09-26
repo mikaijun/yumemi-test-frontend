3c7395bbfd41ab9e2231aee8d57c9e005c9f9c53
mikaijun <mj.tempe443@gmail.com>
2024-09-27 07:50:27 +0900

    feat: #14 レイアウトコンポーネント作成 ヘッダー+main要素+フッターを組み合わせたコンポーネントを作成しました

---
+++ src/components/templates/layout/Layout.stories.tsx
@@ -0,0 +1,34 @@
+import { within, expect } from "@storybook/test";
+import type { Meta, StoryObj } from "@storybook/react";
+import { Layout } from "./Layout";
+
+const meta = {
+  title: "Template/Layout",
+  component: Layout,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+  },
+} satisfies Meta<typeof Layout>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const testHeader: Story["play"] = async ({ canvasElement, step }) => {
+  const canvas = within(canvasElement);
+  await step("Headerが正しく表示されていることを確認", async () => {
+    const header = await canvas.findByRole("header");
+    expect(header).toBeVisible();
+  });
+  await step("Footerが正しく表示されていることを確認", async () => {
+    const footer = await canvas.findByRole("footer");
+    expect(footer).toBeVisible();
+  });
+};
+
+export const Default: Story = {
+  args: {
+    children: <div style={{ padding: "1rem" }}>children</div>,
+  },
+  play: testHeader,
+};
